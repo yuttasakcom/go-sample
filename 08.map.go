d3a23eff2c86f43152ad4c797eddcc6c4ac2a633
YoProgrammer <yuttasakcom@gmail.com>
2018-01-28 14:18:29 +0700

    updatae 08

--- 08.map.go
+++ 08.map.go
@@ -10,8 +10,25 @@ func main() {
 
 	fmt.Println(a)
 
-	x, ok := a["x"]
+	x, ok := a["x"] // ok = false, x =
 
 	fmt.Println(ok)
 	fmt.Println(x)
+
+	for k, v := range a {
+		fmt.Println(k, ":", v)
+	}
+
+	fmt.Println("-----------")
+
+	b := map[string]string{
+		"name": "Gopher",
+		"age":  "35",
+	}
+
+	fmt.Println(b)
+
+	for k, v := range b {
+		fmt.Println(k, ":", v)
+	}
 }
